da590f001d31af6aa9aad14edd950a506c2d1bd7
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-06-06 18:13:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function calculateGrade(percentage) {
   //your code here
+	if(percentage>=90){
+		return("A");
+	}else if(percentage >=80 && percentage<90){
+		return("B");
+	}else if(percentage >=70 && percentage<80){
+		return("C");
+	}else if(percentage >=60 && percentage<70){
+		return("D");
+	} else if(percentage < 60){
+		return("F");
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
